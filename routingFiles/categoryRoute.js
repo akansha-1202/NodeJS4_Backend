42799e1e28afd2b8ff229046ed43e5937d7b7905
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-07-21 21:41:43 +0530

    Add files via upload

--- routingFiles/categoryRoute.js
+++ routingFiles/categoryRoute.js
@@ -1,7 +1,9 @@
 const category = require("../controller/categoryController");
-const authorization = require("../middleware/authorization");
+// const authorization = require("../middleware/authorization");
 
 const categoryRoute = require("express").Router();
 
-categoryRoute.get("/category",authorization, category); //specific middleware
+// categoryRoute.get("/category",authorization, category); //specific middleware
+categoryRoute.get("/category", category);
+
 module.exports = categoryRoute;
